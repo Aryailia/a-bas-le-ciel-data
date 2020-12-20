37291588d961a6464ab81fe1a5cee6162fec10e3
Aryailia <Aryailia@users.noreply.github.com>
2020-12-20 22:20:32 +0800

    Parse subtitles before commiting

--- make.sh
+++ make.sh
@@ -14,6 +14,7 @@ SUBCOMMANDS
   compile           Incrementally update the output json
   force-compile     Rebuild entire output json
   mark-done         Move the files from '\'
+  clean-publish
   compile
   force-compile
   verify-have-subs
@@ -28,7 +29,7 @@ NL='
 
 # --write-auto-sub not working in newest youtube-dl (15 Dec 2020)
 ytdl() {
-  youtube-dlc -4 "$@"
+  youtube-dlc -4 "$@" || exit "$?"
 }
 
 main() {
@@ -38,7 +39,7 @@ main() {
 
   OLD_DIR="./json"
   NEW_DIR="./new"
-  SUB_DIR="./transcripts"
+  SUB_DIR="./subtitles"
   OUT_DIR="./compiled"
   PUBLISH="./publish"
 
@@ -64,27 +65,22 @@ _make() {
   for arg in "$@"; do
     errln "" "Running \`${NAME} ${arg}\`"
     case "${arg}"
-      in help)              show_help
+      in help)                    show_help
 
-      ;; download-channel)
-        mkdir -p "${NEW_DIR}" "${SUB_DIR}"
-        download_by_channel "${CHANNEL_URL}" "${NEW_DIR}" "${SUB_DIR}"
-        download_playlists "${CHANNEL_URL}"
-
-      ;; download-rss)
-        mkdir -p "${NEW_DIR}" "${SUB_DIR}"
-        download_by_rss
-        download_playlists "${CHANNEL_URL}"
-
-      ;; compile)           compile false
-      ;; force-compile)     compile true
-      ;; verify-have-subs)  verify_have_subs
-      ;; supplement-subs)   supplement_subs
+      ;; download-channel)        download_by_channel "${CHANNEL_URL}" \
+                                    "${NEW_DIR}" "${SUB_DIR}"
+      ;; download-rss)            download_by_rss
+      ;; download-playlist-list)  download_playlist_list "${CHANNEL_URL}"
+      ;; compile)                 compile false
+      ;; force-compile)           compile true
+      ;; verify-have-subs)        verify_have_subs
+      ;; supplement-subs)         supplement_subs
+      ;; clean-publish)           rm -r "${PUBLISH}"
 
       ;; cron)
         # 'mark-done' will skip the 'publish' step if no new files
-        #_make download-rss compile mark-done publish
-        # 'false' is just a no-op
+        #_make download-rss download-playlist-list compile mark-done publish
+        #_make prepare-publish clean-publish
 
       ;; mark-done)
         count='0'
@@ -95,16 +91,16 @@ _make() {
         [ "${count}" = '0' ] && die FATAL 1 "No files in '${NEW}' processed"
         for_each "${NEW_DIR}" move_to_old
 
-        
       ;; make-sample)
         jq '[limit(123; .[])]' "${FINAL}" >"../a-bas-le-ciel/video.json"
         cp "${PLAYLIST}" "../a-bas-le-ciel/playlist.json"
 
-      ;; publish)
-        ssh_push_subtree "./compiled" compiled 'a-bas-le-ciel-data' \
-          'git@github.com:Aryailia/a-bas-le-ciel-data.git' \
-          copy_transcripts_to_published
-        # end
+      ;; prepare-publish)
+        rm -r "${PUBLISH}"
+        mkdir -p "${PUBLISH}/transcripts"
+        cp "${FINAL}" "${PUBLISH}/"
+        cp "${PLAYLIST}" "${PUBLISH}/"
+        node './parse-subs.mjs' "${SUB_DIR}" "${PUBLISH}/transcripts"
 
       ;; *)  die FATAL 1 "Inavlid command \`${NAME} ${arg}\`"
     esac
@@ -113,39 +109,40 @@ _make() {
   [ "$#" = 0 ] && { show_help; exit 1; }
 }
 
-ssh_push_subtree() {
-  # $1: the directory
-  # $2: the branch name
-  # $3: private key name
-  # $4: remote ssh url tag
-  # $5: extra commands to run after checks
-
-  [ -n "$( git status --short )" ] && die FATAL 1 \
-    "Please commit changes before updating"
-  [ -d "${publish}" ] && die FATAL 1 \
-    "The directory '${publish}' exists but was reserved"
-
-
-  cp -r "${1}" "${PUBLISH}"
-  "${5}"
-  git branch --force --delete "${2}"
-  git add "${PUBLISH}"
-  git commit -m 'publishing'
-  git subtree split --prefix "${PUBLISH#./}" --branch="${2}"
-  git reset HEAD^
-  rm -r "${PUBLISH}"
-  settings="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-  GIT_SSH_COMMAND="ssh -i '${HOME}/.ssh/${3}' ${settings}" \
-    git push -f "${4}" "${2}"
-}
-
-copy_transcripts_to_published() {
-  # Github uses the actions on the branch that is being pushed
-  # See: https://stackoverflow.com/questions/64565482
-  #cp -r ".github" "${PUBLISH}/";
-  #cp -r "${SUB_DIR}" "${PUBLISH}/";
-  :
-}
+  ## Github uses the actions on the branch that is being pushed
+  ## Thus '.github/workflows' exists within ${PUBLISHED}
+  ## See: https://stackoverflow.com/questions/64565482
+  #ssh_push_subtree "./compiled" compiled 'a-bas-le-ciel-data' \
+  #  'git@github.com:Aryailia/a-bas-le-ciel-data.git' \
+  #  parse_subs_to_transcripts \
+  ## end
+#ssh_push_subtree() {
+#  # $1: the directory
+#  # $2: the branch name
+#  # $3: private key name
+#  # $4: remote ssh url tag
+#  # $5: extra commands to run after checks
+#
+#  [ -n "$( git status --short )" ] && die FATAL 1 \
+#    "Please commit changes before updating"
+#  [ -d "${publish}" ] && die FATAL 1 \
+#    "The directory '${publish}' exists but was reserved"
+#
+#
+#  rm -r "${PUBLISH}" 2>/dev/null
+#  cp -r "${1}" "${PUBLISH}"
+#  "${5}"
+#  exit
+#  git branch --force --delete "${2}"
+#  git add "${PUBLISH}"
+#  git commit -m 'publishing'
+#  git subtree split --prefix "${PUBLISH#./}" --branch="${2}"
+#  git reset HEAD^
+#  rm -r "${PUBLISH}"
+#  settings="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+#  GIT_SSH_COMMAND="ssh -i '${HOME}/.ssh/${3}' ${settings}" \
+#    git push -f "${4}" "${2}"
+#}
 
 move_to_old() {
   mv "${NEW_DIR}/${1}" "${OLD_DIR}/${1}" || exit "$?"
@@ -211,7 +208,7 @@ _playlists_to_json() {
   <&0 sed 's/$/,/' | sed '$s/,$//'
   printf %s ']'
 }
-download_playlists() {
+download_playlist_list() {
   #$1: channel url
   ytdl --ignore-errors --dump-json --flat-playlist \
     "${1}/playlists" \
@@ -238,7 +235,6 @@ compile() {
     cp "${FINAL}" "${INTERIM}" || exit "$?"
   fi
   errln "Outputting to '${OUT_DIR}/video.json'"
-  mkdir -p "${OUT_DIR}" "${NEW_DIR}"
   join "${INTERIM}" "${NEW_DIR}" >"${OUT_DIR}/video.json"
 
   #####
---
+++ parse-subs.mjs
@@ -0,0 +1,70 @@
+import { promises as Fs } from 'fs';
+//run: node % transcripts out
+
+const sub_dir = process.argv[2];
+const out_dir = process.argv[3];
+
+const webttv_list = await Fs.readdir(sub_dir);
+try {
+  await Fs.mkdir(out_dir);
+} catch (err) {
+  if (err.code != 'EEXIST') {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+const length = webttv_list.length;
+const result = new Array(length);
+let index = 0;
+for (let i = 0; i < length; ++i) {
+  const filename = webttv_list[i];
+  if (filename.endsWith(".vtt")) {
+    result[index] = Fs.readFile(`${sub_dir}/${filename}`, "UTF8")
+      .then(parse_webvtt)
+      .then(transcript => Fs.writeFile(`${out_dir}/${filename}`, transcript, "UTF8"));
+    ++index;
+  }
+}
+
+function parse_webvtt(input_string) {
+  if (typeof input_string !== "string") {
+    throw new ParseError("Not a string");
+  }
+  // Guard against Windows "/r/n" (probably not necessary)
+  input_string = input_string.replace(/\r\n|\r/, "\n");
+  // Delimiter is a blank line
+  const input = input_string.split('\n\n');
+
+  if (!input[0].startsWith("WEBVTT") && !input[0].substring(1).startsWith("WEBVTT")) {
+    throw new ParseError("Invalid vtt format: Does not start with 'WEBVTT' or '[BOM]WEBVTT'. [BOM] is a single byte");
+  }
+
+  const cues = parse_cues(input, 1);
+  return cues.join("");
+}
+
+function parse_cues(input, start) {
+  const length = input.length;
+  //const output = new Array(Math.ceil(length / 2));
+  const output = new Array(length - 1);
+  let index = 0;
+  let cache = " ";
+  for (; start < length; ++start) {
+    const cue = input[start].split("\n");
+    if (cache != cue[2] && typeof cue[2] === "string") {
+      cache = cue[2];
+      //output[index] = cache;
+      output[index] = cue[2].replace(/<[^>]*>/g, "");
+      index += 1;
+    }
+  }
+  //if (length > 0) {
+  //  const cue = input[length - 1].split("\n");
+  //  console.log(cue);
+  //  //output[index] = cue[2].replace(/<.*>/g, "");
+  //}
+  return output.slice(0, index);
+}
+
+
